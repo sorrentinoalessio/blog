a6ac0035f0b1054d8dac857a8cc2e651b8a5a2ce
Alessio Sorrentino <sorrentinoalessio85@gmail.com>
2026-01-22 23:20:19 +0100

    creazione validator addUser

---
+++ src/validators/addUserValidator.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import expressJoi from 'express-joi-validation';
+
+const createValidator = expressJoi.createValidator;
+const validator = createValidator({ passError: true});
+
+const bodyValidator = Joi.object({
+    name: Joi.string().required().min(3).max(256),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(3)
+})
+
+export const addUserValidator = validator.body(bodyValidator);
